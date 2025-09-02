aa4eb103254b7df6386377d44b49ff635b06805b
AcWrong02 <3024800887@qq.com>
2025-09-02 20:16:04 +0800

    feat(editor): open codeBlock feature

--- src/features/editor/extensions/base.ts
+++ src/features/editor/extensions/base.ts
@@ -11,7 +11,6 @@ import SlashCommand from "./command/slash";
 export const baseExtensions = [
 	StarterKit.configure({
 		heading: { levels: [1, 2, 3] },
-		codeBlock: false,
 		blockquote: {
 			HTMLAttributes: {
 				class: "custom-blockquote",
