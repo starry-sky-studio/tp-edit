b0c98ce718c0edb23c0eaac9371e090564801d8c
Upwards <1512501934@qq.com>
2025-10-24 10:36:43 +0800

    refactor(editor): remove unused InsertHandles component; update table row addition logic and clean up page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import Tiptap from "@/features/editor/Tiptap";
-import { useUserUIStore } from "@/stores/userStore";
 
 export default function Home() {
 	return (
 		<div className="h-screen w-screen overflow-auto bg-gray-50 flex justify-center items-start">
-			{useUserUIStore.getState().showCustomHeader ? "1" : "0"}
-			<button>toggler</button>
 			<Tiptap />
 		</div>
 	);
